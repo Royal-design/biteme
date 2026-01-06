b1ff9f376201db317b7d66008179fa3b54fe8020
Royal-design <royaldesign1999@gmail.com>
2026-01-06 13:25:18 +0100

    home page ongoing

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 text-xl">
-      <Text className="text-red-600 text-3xl">Home Pages</Text>
+    <View className="flex-1 justify-center items-center bg-amber-500">
+      <View className="bg-white/20 p-10 rounded-full">
+        <View className="bg-white/20 p-8 rounded-full">
+          <Image
+            source={require("../assets/foodie.png")}
+            accessibilityLabel="food bg"
+            className="h-52 w-52"
+          />
+        </View>
+      </View>
+      <View className="items-center space-y-4 mt-10">
+        <Text className="text-7xl font-bold tracking-widest text-white">
+          Biteme
+        </Text>
+        <Text className="text-white tracking-widest font-medium">
+          Food is always right
+        </Text>
+      </View>
     </View>
   );
 }
