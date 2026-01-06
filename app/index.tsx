0d414aefe71a14159a3a8706e4b2e6e410dd0091
Royal-design <royaldesign1999@gmail.com>
2026-01-06 12:46:27 +0100

    home page ongoing

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1">
-      <Text>Home Page</Text>
+    <View className="flex-1 text-xl">
+      <Text className="text-red-600 text-3xl">Home Pages</Text>
     </View>
   );
 }
