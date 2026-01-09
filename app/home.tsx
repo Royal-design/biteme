e64a2263b6261976654851350bab0a533a1a6443
Royal-design <royaldesign1999@gmail.com>
2026-01-09 11:46:48 +0100

    ongoing

---
+++ app/home.tsx
@@ -0,0 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, ScrollView, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex-1">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-4"
+      >
+        <View className="flex-row justify-between items-center">
+          <Image
+            source={{ uri: "https://i.pravatar.cc/150?img=2" }}
+            accessibilityLabel="Avatar"
+          />
+          <Ionicons name="notifications" />
+        </View>
+      </ScrollView>
+
+      <StatusBar style="dark" />
+    </SafeAreaView>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,63 @@
-import React from "react";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import React, { useEffect } from "react";
 import { Image, Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 export default function Index() {
+  const ring1Padding = useSharedValue(0);
+  const ring2Padding = useSharedValue(0);
+  const router = useRouter();
+
+  const startAnimation = () => {
+    ring1Padding.value = 0;
+    ring2Padding.value = 0;
+
+    ring1Padding.value = withSpring(40, {
+      damping: 12,
+      stiffness: 120,
+    });
+
+    setTimeout(() => {
+      ring2Padding.value = withSpring(32, {
+        damping: 12,
+        stiffness: 120,
+      });
+    }, 200);
+  };
+
+  useEffect(() => {
+    startAnimation();
+    setTimeout(() => {
+      router.replace("/home");
+    }, 2500);
+  }, []);
+
+  const ring1Style = useAnimatedStyle(() => ({
+    padding: ring1Padding.value,
+  }));
+
+  const ring2Style = useAnimatedStyle(() => ({
+    padding: ring2Padding.value,
+  }));
+
   return (
     <View className="flex-1 justify-center items-center bg-amber-500">
-      <View className="bg-white/20 p-10 rounded-full">
-        <View className="bg-white/20 p-8 rounded-full">
+      <Animated.View style={ring1Style} className="bg-white/20 rounded-full">
+        <Animated.View style={ring2Style} className="bg-white/20 rounded-full">
           <Image
             source={require("../assets/foodie.png")}
             accessibilityLabel="food bg"
             className="h-52 w-52"
           />
-        </View>
-      </View>
+        </Animated.View>
+      </Animated.View>
+
       <View className="items-center space-y-4 mt-10">
         <Text className="text-7xl font-bold tracking-widest text-white">
           Biteme
@@ -21,6 +66,7 @@ export default function Index() {
           Food is always right
         </Text>
       </View>
+      <StatusBar style="light" />
     </View>
   );
 }
